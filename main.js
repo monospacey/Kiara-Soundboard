4375b1c96baf7c8d57f6acfbdfc08b5265eb23a4
btang164 <>
2024-04-10 14:36:09 +1000

    Bugfix: call audio?

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
-import { playAudio } from './playAudio.js'
+import "./style.css";
+import javascriptLogo from "./javascript.svg";
+import viteLogo from "/vite.svg";
+import eggOoAudio from "/egg-oo.mp3";
+import { setupCounter } from "./counter.js";
+import { playAudio } from "./playAudio.js";
 
-document.querySelector('#app').innerHTML = `
+document.querySelector("#app").innerHTML = `
   <div>
     <a href="https://vitejs.dev" target="_blank">
       <img src="${viteLogo}" class="logo" alt="Vite logo" />
@@ -23,13 +24,12 @@ document.querySelector('#app').innerHTML = `
       Click on the Vite logo to learn more
     </p>
   </div>
-`
+`;
 
-setupCounter(document.querySelector('#counter'));
+setupCounter(document.querySelector("#counter"));
 
 let eggButton = document.getElementById("egg");
 
 eggButton.addEventListener("click", () => {
-  playAudio("./public/egg-oo.mp3")
-}
-);
+  playAudio(eggOoAudio);
+});
